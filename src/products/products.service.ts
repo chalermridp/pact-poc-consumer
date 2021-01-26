d956a01e3b18cb4e5cdd182b14b31cf8173e5aed
Chalermrid Patipanang <chalermrid.patipanang@tesco.com>
2021-01-27 00:42:18 +0700

    error handling for get by id

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
-import { NotFoundException } from 'src/common/exceptions/not-found.exception';
+import { BaseException } from 'src/common/exceptions/base.exception';
 import { ProductsModel } from './products.model';
 
 @Injectable()
@@ -18,8 +18,15 @@ export class ProductsService {
   }
 
   async getById(id: string): Promise<ProductsModel> {
-    const response = await axios.get(`${this.apiUrl}/products/${id}`);
-    //todo handle product not found error
-    return response.data;
+    try {
+      const response = await axios.get(`${this.apiUrl}/products/${id}`);
+      return response.data;
+    } catch (error) {
+      throw new BaseException(
+        error.response.data.code,
+        error.response.data.error_name,
+        error.response.data.error_message,
+      );
+    }
   }
 }
